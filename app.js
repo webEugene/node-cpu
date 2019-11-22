50a2be7e718365448016fdcec106ae41b6c280b7
richierich <richierich@syneforge.com>
2019-11-22 14:18:19 +0200

    add sass styles

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const sassMiddleware = require('node-sass-middleware');
+const postcssMiddleware = require('postcss-middleware');
+const autoprefixer = require('autoprefixer');
 const path = require('path');
 const app = express();
 
 const index = require('./routes/index');
 // middleware
+app.use(
+    sassMiddleware({
+        src: __dirname + '/sass', 
+        dest: __dirname + '/public/stylesheets',
+        debug: true,
+        // outputStyle: 'compressed',
+        prefix:  '/stylesheets'   
+    })
+ );
+//  app.use(postcssMiddleware({
+//     plugins: [
+//       /* Plugins */
+//       autoprefixer({
+//         prefixes: {flexbox: true}
+//       })
+//     ],
+//     src: function(req) {
+//       return path.join(__dirname + '/sass', req.url);
+//     }
+//   }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 
